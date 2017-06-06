5a1d20c2eac06e13c182ed8b145a6c86d38238b3
Nikita Svetlov <svetlov.n@gmail.com>
2017-06-07 00:14:29 +0300

    adding comments

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var background = window.getComputedStyle(document.body).backgroundColor;
 var filling = 'black';
 var core, pattern, interval, indexOfPat, direction = 0; active = [], score, speed;
 
+// В массиве хранятся паттерны для отрисовки фигур в разных положениях
 var figures = [
 [[-10, -9, 1], [-10, 10, 20], [-10, -1, 1], [-1, 10, 11], [-9, -1, 1], [1, 9, 10], [-11, -1, 1]],
 [[-10, -9, 1], [-1, 1, 2], [-10, 1, 10], [-10, -1, 9], [-10, 10, 11], [-11, -1, 10], [-10, -9, 10]],
@@ -20,6 +21,7 @@ var result = document.querySelector('#result');
 startButton.addEventListener('click', startGame);
 document.addEventListener('keydown', moveCore);
 
+// Если приложение открыто на мобильном устройстве, отображаем кнопки управления
 if (screen.width > 1000) {
   document.querySelector('#keyboard').classList.add('hidden');
 }
@@ -32,6 +34,7 @@ function rand(x) {
   return Math.round(Math.random() * x);
 }
 
+// Функция для начала игры - скрываем подсказку, обнуляем счёт
 function startGame() {
   document.querySelector('#controls').classList.add('hidden');
   startButton.classList.toggle('hidden');
@@ -46,6 +49,7 @@ function startGame() {
   generateFigure();
 }
 
+// Функция завершения игры - прячем поле, выводим счёт
 function gameOver() {
   scoreLine.classList.toggle('hidden');
   field.classList.toggle('hidden');
@@ -58,6 +62,7 @@ function gameOver() {
   startButton.classList.toggle('hidden');
 }
 
+// Функция для генерации случайной фигуры, и определения точки старта для неё
 function generateFigure() {
   indexOfPat = rand(6);
   if (indexOfPat >= 5) {
@@ -70,6 +75,7 @@ function generateFigure() {
   dropFigure(pattern);
 }
 
+// Функция для отрисовки фигуры. Принимает точку ядра, вокруг которого отрисовывает фигуру по заданному паттерну. Помечает клетки, которые занимает фигура как активные
 function displayFigure(index) {
   cells[index].style.backgroundColor = filling;
   cells[index].classList.add('active');
@@ -80,6 +86,7 @@ function displayFigure(index) {
   active = Array.from(document.querySelectorAll('.active'));
 }
 
+// Функция для удаления фигуры с поля
 function clearFigure(index) {
   cells[index].style.backgroundColor = background;
   cells[index].classList.remove('active');
@@ -89,14 +96,18 @@ function clearFigure(index) {
   }
 }
 
+// Функция движения фигуры вниз
 function dropFigure(pattern) {
+  // Проверяем, есть ли под фигурой заполненные клетки или конец поля
   if (checkSpace()) {
+    // Если заполненные клетки есть, а при этом фигура находится на точке старта, заканчиваем игру, выводим счёт
     if (core < 30) {
       core = null;
       pattern = null;
       gameOver();
       return;
     }
+    // Если есть касание с заполненными клетками, убираем отметку с активных клеток, запускаем проверку на составленную линию и генерируем следующую фигуру
     displayFigure(core);
     core = null;
     cells.forEach(cell => {cell.classList.remove('active')});
@@ -105,6 +116,7 @@ function dropFigure(pattern) {
     generateFigure();
     return;
   }
+  // Если под фигурой всё свободно, то сдвигаем ядро и отрисовываем её, и удаляем через заданный промежуток времени (в зависимости от скорости). После чего запускаем функцию снова
   core += 10;
   displayFigure(core);
   let step = new Promise((done, fail) => {
@@ -119,6 +131,56 @@ function dropFigure(pattern) {
   return;
 }
 
+// Функция для проверки места под активной фигурой
+function checkSpace() {
+  if (active) {
+    for (let cell of active) {
+      let index = cells.indexOf(cell);
+      if (!cells[index + 10]) {
+        return true;
+      }
+      if (cells[index + 10].style.backgroundColor === filling && !cells[index + 10].classList.contains('active')) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+// Функция для проверки составленных рядов
+function checkLines() {
+  for (let i = 0; i < cells.length; i += 10) {
+    let lineReady = cells.slice(i, i + 10)
+    .map(cell => {return cell.style.backgroundColor})
+    .every(cell => {return cell === filling});
+    if (lineReady) {
+      deleteRow(i);
+      updateScore();
+    }
+  }
+}
+
+// Функция для удаления из DOM заполненных рядов, и генерации новых пустых на их месте
+function deleteRow(index) {
+  cells[index].parentNode.remove();
+  let newRows = document.createElement('tr');
+  newRows.innerHTML = '<td class = "left"></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td class = "right"></td>';
+  field.insertBefore(newRows, field.firstChild);
+  cells = Array.from(document.querySelectorAll('td'));
+}
+
+// Функция для обновления счёта и скорости игры (максимальная скорость - 10, 0.1сек на 1 шаг)
+function updateScore() {
+  score += 100;
+  scoreBoard.innerHTML = score;
+  if (score % 500 === 0 && interval > 100) {
+    interval -= 100;
+    speed++;
+    speedBoard.innerHTML = speed;
+  }
+}
+
+// Фунция для управления фигурами с клавиатуры
 function moveCore(ev) {
   switch(ev.keyCode) {
   case 37:
@@ -136,6 +198,7 @@ function moveCore(ev) {
   }
 }
 
+// Функция для вращения фигуры - переход к следующему паттерну в массиве
 function turnFigure(ev) {
   ev.preventDefault();
   if ((direction + 1) < 4) {
@@ -148,6 +211,7 @@ function turnFigure(ev) {
     displayFigure(core);
   }
   else {
+    // Когда прокрутили 3 раза, возвращаемся к первому паттерну положения
     if (checkTurn(-3)) {
       return;
     }
@@ -158,52 +222,7 @@ function turnFigure(ev) {
   }
 }
 
-
-function checkLines() {
-  for (let i = 0; i < cells.length; i += 10) {
-    let lineReady = cells.slice(i, i + 10)
-    .map(cell => {return cell.style.backgroundColor})
-    .every(cell => {return cell === filling});
-    if (lineReady) {
-      deleteRow(i);
-      updateScore();
-    }
-  }
-}
-
-function deleteRow(index) {
-  cells[index].parentNode.remove();
-  let newRows = document.createElement('tr');
-  newRows.innerHTML = '<td class = "left"></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td class = "right"></td>';
-  field.insertBefore(newRows, field.firstChild);
-  cells = Array.from(document.querySelectorAll('td'));
-}
-
-function updateScore() {
-  score += 100;
-  scoreBoard.innerHTML = score;
-  if (score % 500 === 0 && interval > 100) {
-    interval -= 100;
-    speed++;
-    speedBoard.innerHTML = speed;
-  }
-}
-
-function checkSpace() {
-  if (active) {
-    for (let cell of active) {
-      let index = cells.indexOf(cell);
-      if (!cells[index + 10]) {
-        return true;
-      }
-      if (cells[index + 10].style.backgroundColor === filling && !cells[index + 10].classList.contains('active')) {
-        return true;
-      }
-    }
-  }
-  return false;
-}
-
+// Функция для проверки возможности вращения фигуры - нет ли краёв поля или закрашенных клеток
 function checkTurn(step) {
   let pattern = figures[direction + step][indexOfPat];
   for (let dot of pattern) {
@@ -222,6 +241,7 @@ function checkTurn(step) {
   }
 }
 
+// Функция для сдвига фигуры влево
 function moveLeft() {
   for (let cell of active) {
     if (cell.classList.contains('left')) {
@@ -236,6 +256,7 @@ function moveLeft() {
   displayFigure(core);
 }
 
+// Функция для сдвига фигуры вправо
 function moveRight() {
   for (let cell of active) {
     if (cell.classList.contains('right')) {
@@ -250,6 +271,7 @@ function moveRight() {
   displayFigure(core);
 }
 
+// Функция для ускорения падения фигуры
 function speedUp(ev) {
   ev.preventDefault();
   for (let cell of active) {
